89936b6df52114ba874bb84c773720edcd41c9f7
gpborla <gpborla+github@gmail.com>
2018-09-21 20:20:20 +0800

    Injected HttpClient in HeroService constructor. Wrapped Message Service in a private log method. Defined the heroesUrl with the address of the heroes resource on the server.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,12 +5,18 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) { }
+
+  private heroesUrl = 'api/heroes'; // URL to web api
 
   getHeroes(): Observable<Hero[]> {
     //TODO: send the message _after_ fetching the heroes
@@ -22,5 +28,10 @@ export class HeroService {
     //TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
