707ba4c9579b1242caf9068fe7bf745751429dd0
gpborla <gpborla+github@gmail.com>
2018-09-21 20:04:31 +0800

    Added notes for HttpClientInMemoryWebApiModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,10 @@ import { InMemoryDataService } from './in-memory-data.service';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     )
