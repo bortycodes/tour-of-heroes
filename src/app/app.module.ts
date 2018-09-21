3aef62087877232553fd3393eee4f3a0ef5dbc32
gpborla <gpborla+github@gmail.com>
2018-09-21 20:02:31 +0800

    Import HttpClientInMemoryWebApiModule and InMemoryDataService in App Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,10 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';  
 
 @NgModule({
   declarations: [
@@ -23,7 +26,11 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
